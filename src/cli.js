a7ba2a3462d2a61edab5e54b68545599da094e53
Vera Zhukova <mirvhytri@gmail.com>
2026-03-23 15:55:29 +0100

    fix cli prompt output

--- src/cli.js
+++ src/cli.js
@@ -2,7 +2,8 @@ import readlineSync from 'readline-sync';
 
 const run = () => {
   console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('May I have your name? ');
+  console.log('May I have your name?');
+  const name = readlineSync.question('');
   console.log(`Hello, ${name}!`);
 };
 
