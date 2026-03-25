32eb6e1c65ec852c42a8daddbd5477290dc0f0cb
Vera Zhukova <mirvhytri@gmail.com>
2026-03-25 16:04:19 +0100

    fix: calculator checks only numbers

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -24,7 +24,7 @@ export const main = () => {
         console.log (`Question: ${expression}`)
         const answer = readlineSync.question(`Your answer `)
         const correctAnswer = eval(expression);
-        const result = checkAnswer(answer, correctAnswer, name);
+        const result = checkAnswer(Number(answer), Number(correctAnswer), name);
         if(result === false) {
           return
         }
