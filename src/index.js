9bac740c0375a10f111c9be4e3659f0d7b76af0e
Vera Zhukova <mirvhytri@gmail.com>
2026-03-25 20:50:16 +0100

    fix: semi

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import readlineSync from 'readline-sync';
+import readlineSync from 'readline-sync'
 
 export function greetUser() {
   console.log('Welcome to the Brain Games!')
@@ -10,13 +10,13 @@ export function greetUser() {
 export const generateNumber = () => {
   const number = Math.random()
   return Math.floor(number * 100 + 1)
-};
+}
 
 export const checkAnswer = (answer, correctAnswer, userName) => {
   if (answer === correctAnswer) {
     console.log('Correct!')
     return true
   }
-  console.log(`${answer} is wrong answer ;(. Correct answer was ${correctAnswer}. Let's try again, ${userName}!`)
+  console.log(`${answer} is wrong answer (. Correct answer was ${correctAnswer}. Let's try again, ${userName}!`)
   return false
-};
+}
